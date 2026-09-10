6d8ea5718f4f773715f3582b69b0ec3ded6cec96
Josh Yi <joshua.yi1018@gmail.com>
2026-09-10 18:44:18 -0400

    Implement Google OAuth with Passport

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db');
-const { dir } = require('console');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
+
+// Passport config
+require('./config/passport')(passport);
 
 connectDB();
 
@@ -15,18 +19,31 @@ const app = express();
 
 // Logging
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 
 // Handlebars
 app.engine('.hbs', engine({ defaultLayout: 'main', extname: 'hbs' }));
 app.set('view engine', '.hbs');
 
+// Sessions 
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 5000;
 
--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,15 @@ const router = express.Router();
 // @desc Login/Landing page
 // @route GET /
 router.get('/', (request, response) => {
-    response.render('login', {
-        layout: 'login'
-    });
+  response.render('login', {
+    layout: 'login'
+  });
 })
 
 // @desc Dashboard
 // @route GET /dashboard
 router.get('/dashboard', (request, response) => {
-    response.render('dashboard');
+  response.render('dashboard');
 })
 
 module.exports = router
