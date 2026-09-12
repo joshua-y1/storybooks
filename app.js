fbf3c5a133da195d5ed3b5456a57e5756411681f
Josh Yi <joshua.yi1018@gmail.com>
2026-09-12 02:06:15 -0400

    Add edit and delete story functionality

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const session = require('express-session');
 const { MongoStore } = require('connect-mongo');
@@ -23,13 +24,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Method override
+app.use(methodOverride(function (request, response) {
+  if (request.body && typeof request.body === 'object' && '_method' in request.body) {
+    // look in urlencoded POST bodies and delete it
+    let method = request.body._method;
+    delete request.body._method;
+    return method;
+  };
+}));
+
 // Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 // Handlebars Helper
-const { formatDate, truncate, editIcon } = require('./helpers/hbs');
+const { formatDate, truncate, editIcon, select } = require('./helpers/hbs');
 
 // Handlebars
 app.engine(
@@ -39,6 +50,7 @@ app.engine(
       formatDate,
       truncate,
       editIcon,
+      select,
     },
     defaultLayout: 'main', 
     extname: 'hbs' })
--- routes/stories.js
+++ routes/stories.js
@@ -40,5 +40,68 @@ router.get('/', ensureAuth, async (request, response) => {
   }
 })
 
+// @desc Show edit page
+// @route GET /stories/edit/:id
+router.get('/edit/:id', ensureAuth, async (request, response) => {
+  try {
+    const story = await Story.findOne({
+      _id: request.params.id
+    }).lean();
+
+    if (!story) {
+      return response.render('error/404');
+    };
+
+    if (story.user != request.user.id) {
+      response.redirect('/stories');
+    } else {
+      response.render('stories/edit', {
+          story,
+      });
+    };
+  } catch(err) {
+    console.error(err);
+    return response.render('error/500');
+  };
+});
+
+// @desc Update story
+// @route PUT /stories/:id
+router.put('/:id', ensureAuth, async (request, response) => {
+  try{
+    let story = await Story.findById(request.params.id).lean();
+
+    if (!story) {
+      return response.render('error/404')
+    }
+
+    if (story.user != request.user.id) {
+      response.redirect('/stories');
+    } else {
+      story = await Story.findOneAndUpdate({ _id: request.params.id }, request.body, {
+        new: true,
+        runValidators: true
+      });
+
+      response.redirect('/dashboard');
+    };
+  } catch(err) {
+    console.error(err);
+    return response.render('error/500');
+  };
+});
+
+// @desc Delete story
+// @route DELETE /stories/:id
+router.delete('/:id', ensureAuth, async (request, response) => {
+  try {
+    await Story.findByIdAndDelete({ _id: request.params.id });
+    response.redirect('/dashboard');
+  } catch(err) {
+    console.error(err);
+    return response.render('error/500');
+  };
+});
+
 
 module.exports = router;
